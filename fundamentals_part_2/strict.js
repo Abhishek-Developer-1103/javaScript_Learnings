627c4959544b7e947387110b674a3e8009319874
Abhishek-Developer-1103 <jagdaleabhishek1103@gmail.com>
2026-02-07 19:23:58 +0530

    Manually moved README to root

---
+++ fundamentals_part_2/strict.js
@@ -0,0 +1,19 @@
+//  Strict mode use in the beginning to avoid acc erros
+'use strict';
+//javaScipt  has reserved keywords that we cant use it as variables. 
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest){
+ hasDriversLicense = true;
+}
+if(hasDriversLicense){
+    console.log("I can drive :D");
+}
+
+
+
+
+
+
